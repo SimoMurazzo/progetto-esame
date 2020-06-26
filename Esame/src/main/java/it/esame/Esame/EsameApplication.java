044f93abd976fd81e5cef733bbf174c2fca10f97
Simone Murazzo <simone@archlabs-old>
2020-06-26 18:41:22 +0200

    Inizializzazione

---
+++ Esame/src/main/java/it/esame/Esame/EsameApplication.java
@@ -0,0 +1,13 @@
+package it.esame.Esame;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EsameApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EsameApplication.class, args);
+	}
+
+}
