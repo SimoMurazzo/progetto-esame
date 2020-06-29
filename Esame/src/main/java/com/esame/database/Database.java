686a1e7cabc0efcbe240ee3f5d14a180f4a0809c
SerJoG <sergallet@gmail.com>
2020-06-29 17:59:53 +0200

    Aggiunta variabili per photo classe Event

--- Esame/src/main/java/com/esame/database/Database.java
+++ Esame/src/main/java/com/esame/database/Database.java
@@ -1,5 +1,8 @@
 package com.esame.database;
 
-public class Database {
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
+public class Database {
+	
 }
--- Esame/src/main/java/com/esame/model/Event.java
+++ Esame/src/main/java/com/esame/model/Event.java
@@ -12,12 +12,17 @@ public class Event {
 	private String type;
 	private String intensity;
 	private String date;
-	private String min;
+	private String[] min = new String[2];
+	private String[] max = new String[2];
+	private String[] med = new String[2];
 	
-	public Event(String type, String intensity, String date){
+	public Event(String type, String intensity, String date, String[] min,String[] max, String[] med){
 		this.type = type;
 		this.intensity = intensity;
 		this.date = date;
+		this.min = min.clone();
+		this.max = max.clone();
+		this.med = med.clone();
 	}
 	
 	public void setType(String type) {
@@ -42,5 +47,17 @@ public class Event {
 	
 	public String getDate() {
 		return date;
+	}
+	
+	public String[] getMin() {
+		return min;
+	}
+	
+	public String[] getMax() {
+		return max;
+	}
+	
+	public String[] getMed() {
+		return med;
 	}
 }
