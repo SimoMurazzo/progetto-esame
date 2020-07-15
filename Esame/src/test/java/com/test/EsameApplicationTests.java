78596d57c76f4d4c73f4ea9be6ed38646685ef55
simone <simone@arch-new.localdomain>
2020-07-15 18:58:28 +0200

    Modifica EsameApplicationTests

--- Esame/src/test/java/com/test/EsameApplicationTests.java
+++ Esame/src/test/java/com/test/EsameApplicationTests.java
@@ -2,8 +2,9 @@ package com.test;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import com.esame.EsameApplication;
 
-@SpringBootTest
+@SpringBootTest(classes=com.esame.EsameApplication.class)
 class EsameApplicationTests {
 
 	@Test
